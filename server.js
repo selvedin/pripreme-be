9719aa7e9cc254919f1b275d88d956245a1e5b88
Selvedin <selvedinh@gmail.com>
2020-11-15 01:42:53 +0300

    adding register-login-user-model

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const connectToDB = require('./config/connection')
+
+dotenv.config()
+
 const app = express()
+
+app.use(express.json({ extended: false }))
+
+connectToDB()
+
+app.use('/', require('./routes/main'))
+app.use('/api/users', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3210
 
